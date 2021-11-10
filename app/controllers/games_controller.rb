bb4447f4dd6024894ed19a0ca82acde9b0936374
soonteckling <soonteckl@outlook.com>
2021-11-10 20:07:55 +0800

    added score computer

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,9 +3,14 @@ require 'open-uri'
 class GamesController < ApplicationController
   def new
     @letters = ('a'..'z').to_a.sample(10)
+    @t1 = Time.now
   end
 
   def score
+    @point = 0
+    t2 = Time.now
+    @delta = t2 - params[:time_now].to_time
+    @point += @delta.round/10
     url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
     user_serialized = URI.open(url).read
     user = JSON.parse(user_serialized)
@@ -18,11 +23,14 @@ class GamesController < ApplicationController
 
     if user["found"] && word_array.all? { |char| random_letters.include?(char)}
       @output_message = "Congratulations! #{params[:word].upcase} is a valid English word!"
+      @point += 1
     elsif user["found"] && word_array.all? { |char| random_letters.include?(char)} == false
       # binding.pry
       @output_message = "Sorry but #{params[:word].upcase} can't be built out of #{params[:tags_list].upcase.split("").join(", ")}"
+      @point = 0
     else
       @output_message = "Sorry but #{params[:word].upcase} does not seem to be a valid English word"
+      @point = 0
     end
 
   end
